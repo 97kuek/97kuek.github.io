4b7ba19e1cabd7af8da8fce32b4c601629688e30
Auto Git <auto@example.com>
2026-03-09 17:22:45 +0900

    feat: add Giscus and Expressive Code, fix missing images

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
+import astroExpressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://97kuek.github.io",
   base: "/my-portfolio",
 
-  integrations: [react(), markdoc()],
+  integrations: [astroExpressiveCode(), react(), markdoc()],
 
   vite: {
     // @ts-ignore
