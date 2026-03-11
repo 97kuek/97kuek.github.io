2a8f04c6314cf510f22bd4355af7c36ff7ae7b0e
Auto Git <auto@example.com>
2026-03-11 19:29:51 +0900

    WorkとHackathon非表示

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig({
 
   markdown: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
+    rehypePlugins: [[rehypeKatex, { output: "html" }]],
   },
 
   vite: {
