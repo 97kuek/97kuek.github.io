d215d07b09196526a44f4e2af7cd5735bcb3a501
Auto Git <auto@example.com>
2026-04-24 09:55:15 +0900

    globalcssを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
@@ -28,7 +27,6 @@ export default defineConfig({
   },
 
   vite: {
-    // @ts-ignore
     plugins: [tailwindcss()],
   },
 
